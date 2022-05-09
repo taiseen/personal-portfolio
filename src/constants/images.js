0e474a1aa54bb816f7a38bb835075bf75b765bc1
taiseen <taiseen.cse@gmail.com>
2022-05-10 01:06:37 +0600

    CV add ==> OK

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,7 @@
 import me from '../assets/me.jpg';
 import up from '../assets/up.png';
 import certificate from '../assets/certificate.jpg';
-import cv from '../assets/Demo-CV.pdf';
+import cv from "../assets/Taiseen's CV .pdf";
 
 
 const images = {
