c5df780e4cef76f016273e0f3afa737a5409c577
taiseen <taiseen.cse@gmail.com>
2024-06-10 05:23:45 +0600

    fix: vite config for opening specific port number

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // for development mode
+  server: {
+    port: 8585,
+    open: true, // open browser
+  },
+
 })
